ea284791caee31eb02717a1bd3fff6edee3e9184
Rowan Crawford <rowan.crawford@joukou.co>
2014-04-01 09:50:18 +1300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require( 'gulp' ),
+    huxley = require( './index' );
+
+gulp.task( 'test', function() {
+  huxley();
+});
---
+++ index.js
@@ -0,0 +1,30 @@
+var httpServer = require( 'http-server' ),
+    remote = require( 'selenium-webdriver/remote' ),
+    jar = require( 'selenium-server-standalone-jar' ),
+    huxley = require( 'huxley' );
+
+module.exports = function( options, cb ) {
+  options = options || {};
+  cb = cb || function() {};
+
+  var server = httpServer.createServer(),
+      port = options.port || 8000,
+      huxleyGlob = options.huxleyGlob || '';
+
+  server.listen(port, function() {
+    try {
+      selenium = new remote.SeleniumServer( jar.path, {
+        port: 4444
+      } );
+      selenium.start();
+      // Use defaults, code doesn't allow varargs unfortunately
+      huxley.playbackTasksAndCompareScreenshots( '', '', huxleyGlob, function( err ) {
+        selenium.stop();
+        server.close();
+        cb( err );
+      });
+    } catch (e) {
+      cb( e );
+    }
+  });
+};
