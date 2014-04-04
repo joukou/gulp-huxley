12dd7237a0104e6e0e38d37532dccfddc4cbfbf9
Tony Gaskell <thgaskell@gmail.com>
2014-04-03 22:03:14 -1000

    Decouple http server and selenium
    closes #1
    - remove http and selenium from index.js
    - add http and selenium to gulpfile.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var httpServer = require( 'http-server' ),
-    remote = require( 'selenium-webdriver/remote' ),
-    jar = require( 'selenium-server-standalone-jar' ),
-    huxley = require( 'huxley' ),
+var huxley = require( 'huxley' ),
     through = require( 'through2' ),
     path = require( 'path' ),
     gutil = require( 'gulp-util' );
@@ -9,15 +6,11 @@ var httpServer = require( 'http-server' ),
 module.exports = function( options ) {
   options = options || {};
 
-  var server,
-      root = options.root || '../..',
-      port = options.port || 8000,
-      browser = options.browser,
+  var browser = options.browser,
       serverUrl = options.server,
       driver = options.driver,
       action = null,
-      paths = [],
-      selenium = null;
+      paths = [];
 
   if (typeof driver === 'function') {
     huxley.injectDriver(options.driver);
@@ -34,44 +27,21 @@ module.exports = function( options ) {
       action = huxley.playbackTasksAndCompareScreenshots;
   }
 
-  // Create HTTP server
-  server = httpServer.createServer({
-    root: root
-  });
-
-  server.listen( port, '', function() {
-      try {
-        // Create Selenium server
-        selenium = new remote.SeleniumServer( jar.path, {
-          port: 4444
-        } );
-        selenium.start();
-      } catch (err) {
-        selenium.stop();
-        server.close();
-        callback(err);
-      }
-    });
-
   return through.obj( function( file, enc, callback ) {
     paths.push( path.dirname( file.path ) );
     this.push( file );
     callback();
-  }, function(callback) {
+  }, function( callback ) {
     try {
       action( browser, serverUrl, paths, function( err ) {
         if ( err ) {
           gutil.log( err );
         }
-        selenium.stop();
-        server.close();
       });
     } catch ( err ) {
-      this.emit('error', new gutil.PluginError('gulp-huxley', {
+      this.emit( 'error', new gutil.PluginError('gulp-huxley', {
         message: err
       }));
-      selenium.stop();
-      server.close();
     }
     callback();
   } );
