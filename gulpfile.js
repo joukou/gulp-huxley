4b78c144cb591c8d3bec6116a5677097c48996cb
Rowan Crawford <rowan.crawford@joukou.co>
2014-04-09 07:27:15 +1200

    Syntax inconsistencies.

--- gulpfile.js
+++ gulpfile.js
@@ -7,24 +7,22 @@ var gulp = require( 'gulp' ),
 
 var selenium = null;
 
-gulp.task('http', function() {
-    HttpServer.createServer().listen(8000);
-});
+gulp.task( 'http', function() {
+  HttpServer.createServer().listen( 8000 );
+} );
 
-gulp.task('selenium', function() {
-    selenium = new SeleniumServer(jar.path, {
-        port: 4444
-    });
-    selenium.start();
-});
+gulp.task( 'selenium', function() {
+  selenium = new SeleniumServer( jar.path, { });
+  selenium.start();
+} );
 
-gulp.task( 'test', ['selenium'], function() {
-  gulp.src('./test/**/Huxleyfile.json')
-    .pipe( huxley( {
-        action: 'record',
-        browser: 'chrome',
-        server: selenium.address()
-    } ) );
+gulp.task( 'test', [ 'selenium' ], function() {
+  gulp.src( './test/**/Huxleyfile.json' )
+  .pipe( huxley( {
+    action: 'record',
+    browser: 'chrome',
+    server: selenium.address()
+  } ) );
 });
 
 gulp.task( 'default', [ 'http', 'test' ] );
